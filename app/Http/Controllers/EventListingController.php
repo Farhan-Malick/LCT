e6e362cb0d1d7e1fa8a750fb9c2b25e6878e7366
Usama Ayub <usama@youcloudtech.com>
2022-12-20 01:52:55 +0500

    Changed Ticket listing to Event listing

--- app/Http/Controllers/EventListingController.php
+++ app/Http/Controllers/EventListingController.php
@@ -4,21 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
-use App\Models\TicketListing;
+use App\Models\EventListing;
 use App\Models\Category;
 
-class TicketListingController extends Controller
+class EventListingController extends Controller
 {
-    public function ticketListingForm()
+    public function EventListingForm()
     {
         $categories = Category::all();
         $events = Event::all();
         return view("Admin/pages/ticket_ListingForm",compact('categories','events'));
     }
 
-    public function ticketListing(Request $request){
+    public function EventListing(Request $request){
         
-        $ticket= new TicketListing();
+        $ticket= new EventListing();
         $ticket->title=$request->title;
         $ticket->event_id=$request->event;
         $ticket->status=$request->status;
@@ -29,18 +29,18 @@ class TicketListingController extends Controller
     }
     public function showListing()
     {
-        $listing = TicketListing::all();
+        $listing = EventListing::all();
         return view("Admin/pages/ticket_Listing",compact('listing'));
     }
     public function editListing($id){
         $events = Event::all();
-        $listings = ticketListing::find($id);
+        $listings = EventListing::find($id);
         return view('Admin/pages/editListing',compact('listings','events'));
     }
 
     public function updateListing(Request $request,$id){
         //return $request;
-        $listings = ticketListing::find($id);
+        $listings = EventListing::find($id);
         $listings->title = $request->title;
         $listings->event_id=$request->event;
         $listings->status=$request->status;
@@ -51,7 +51,7 @@ class TicketListingController extends Controller
     }
     public function delete(Request $request,$id){
         
-        $listings = ticketListing::find($id);
+        $listings = EventListing::find($id);
         $listings->delete();
         $request->session()->flash('msg','Data Has Been Deleted Successfully'); 
         return redirect()->back();
